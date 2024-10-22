bd9b0aad0f091dd94c081246a27a73338b16b096
Tganeshreddy36 <ganeshreddyt2@gmail.com>
2024-10-22 11:03:28 +0530

    print th string .py

---
+++ print th string .py
@@ -0,0 +1,15 @@
+"""
+ Write a program to print th length of the given string.
+Sample Input:
+Hello
+"""
+"""
+# Get input from the user
+input_string = input("Enter a string: ")
+
+# Calculate the length of the string
+length_of_string = len(input_string)
+
+# Print the length of the string
+print("Length of the given string:", length_of_string)
+"""
