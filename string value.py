8e5d6683ade4d47ffad3e43597176656c90be657
Tganeshreddy36 <ganeshreddyt2@gmail.com>
2024-10-22 11:00:21 +0530

    Create string value.py

---
+++ string value.py
@@ -0,0 +1,14 @@
+"""
+ Write a program to get a string value and print the same as output:
+Sample Input:
+Hello World
+Sample Output:
+Hello World
+"""
+"""
+# Get input from the user
+input_string = input("Enter a string: ")
+
+# Print the input string
+print(input_string)
+"""
